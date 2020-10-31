de3997ea6496a144aee195052aac4b6f629c2eb3
ryan hatfield <hatfield086@gmail.com>
2020-10-31 11:22:26 -0500

    merge

--- public/js/API.js
+++ public/js/API.js
@@ -1,4 +1,19 @@
-<<<<<<< HEAD
+/* eslint-disable prefer-const */
+/* eslint-disable no-unused-vars */
+const question = document.getElementById("question");
+const choices = Array.from(document.getElementsByClassName("choice-text"));
+const progressText = document.getElementById("progressText");
+const scoreText = document.getElementById("score");
+const progressBarFull = document.getElementById("progressBarFull");
+const scoreDiv = document.getElementById("hud-item");
+let currentQuestion = {};
+let acceptingAnswers = false;
+let score = 0;
+let questionCounter = 0;
+let availableQuesions = [];
+
+let questions = [];
+//API call for Sports category
 fetch("https://opentdb.com/api.php?amount=5&category=21&type=multiple")
   .then(res => {
     return res.json();
@@ -8,33 +23,6 @@ fetch("https://opentdb.com/api.php?amount=5&category=21&type=multiple")
       const formattedQuestion = {
         question: loadedQuestion.question
       };
-=======
-const question = document.getElementById('question');
-const choices = Array.from(document.getElementsByClassName('choice-text'));
-const progressText = document.getElementById('progressText');
-const scoreText = document.getElementById('score');
-const progressBarFull = document.getElementById('progressBarFull');
-const scoreDiv = document.getElementById("hud-item");
-let currentQuestion = {};
-let acceptingAnswers = false;
-let score = 0;
-let questionCounter = 0;
-let availableQuesions = [];
-
-let questions = [];
-
-fetch(
-    'https://opentdb.com/api.php?amount=5&category=21&type=multiple'
-)
-    .then((res) => {
-        return res.json();
-    })
-    .then((loadedQuestions) => {
-        questions = loadedQuestions.results.map((loadedQuestion) => {
-            const formattedQuestion = {
-                question: loadedQuestion.question,
-            };
->>>>>>> origin/main
 
       const answerChoices = [...loadedQuestion.incorrect_answers];
       formattedQuestion.answer = Math.floor(Math.random() * 4) + 1;
@@ -51,6 +39,7 @@ fetch(
       return formattedQuestion;
     });
   });
+//API call for History category
 fetch("https://opentdb.com/api.php?amount=5&category=23&type=multiple")
   .then(res => {
     return res.json();
@@ -76,6 +65,7 @@ fetch("https://opentdb.com/api.php?amount=5&category=23&type=multiple")
       return formattedQuestion;
     });
   });
+//API call for Cars category
 fetch("https://opentdb.com/api.php?amount=5&category=28&type=multiple")
   .then(res => {
     return res.json();
@@ -101,6 +91,7 @@ fetch("https://opentdb.com/api.php?amount=5&category=28&type=multiple")
       return formattedQuestion;
     });
   });
+//API call for Geography category
 fetch("https://opentdb.com/api.php?amount=5&category=22&type=multiple")
   .then(res => {
     return res.json();
@@ -126,6 +117,7 @@ fetch("https://opentdb.com/api.php?amount=5&category=22&type=multiple")
       return formattedQuestion;
     });
   });
+//API call for Random category
 fetch("https://opentdb.com/api.php?amount=5&type=multiple")
   .then(res => {
     return res.json();
