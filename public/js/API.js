889ea78e10b9d3ff28f55fdb623e9235e9f4d85e
puredemon <umaga1031@yahoo.com>
2020-10-31 10:57:05 -0500

    api update

--- public/js/API.js
+++ public/js/API.js
@@ -1,3 +1,17 @@
+const question = document.getElementById('question');
+const choices = Array.from(document.getElementsByClassName('choice-text'));
+const progressText = document.getElementById('progressText');
+const scoreText = document.getElementById('score');
+const progressBarFull = document.getElementById('progressBarFull');
+const scoreDiv = document.getElementById("hud-item");
+let currentQuestion = {};
+let acceptingAnswers = false;
+let score = 0;
+let questionCounter = 0;
+let availableQuesions = [];
+
+let questions = [];
+
 fetch(
     'https://opentdb.com/api.php?amount=5&category=21&type=multiple'
 )
