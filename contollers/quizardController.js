f89e53b34888e202cf3592a8882a6a84b8fa3ef1
Ben Townsend <rbt000011@gmail.com>
2020-10-31 13:53:18 -0500

    all new stuff

--- contollers/quizardController.js
+++ contollers/quizardController.js
@@ -6,8 +6,8 @@ var router = express.Router();
 var cat = require("../models/cat.js");
 
 // Create all our routes and set up logic within those routes where required.
-router.get("/", function(req, res) {
-  cat.all(function(data) {
+router.get("/", (req, res) => {
+  cat.all(data => {
     var hbsObject = {
       cats: data
     };
@@ -16,44 +16,44 @@ router.get("/", function(req, res) {
   });
 });
 
-router.post("/api/cats", function(req, res) {
-  cat.create([
-    "name", "sleepy"
-  ], [
-    req.body.name, req.body.sleepy
-  ], function(result) {
+router.post("/api/cats", (req, res) => {
+  cat.create(["name", "sleepy"], [req.body.name, req.body.sleepy], (
+    result
+  ) => {
     // Send back the ID of the new quote
     res.json({ id: result.insertId });
   });
 });
 
-router.put("/api/cats/:id", function(req, res) {
+router.put("/api/cats/:id", (req, res) => {
   var condition = "id = " + req.params.id;
 
   console.log("condition", condition);
 
-  cat.update({
-    sleepy: req.body.sleepy
-  }, condition, function(result) {
-    if (result.changedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
-      return res.status(404).end();
-    } else {
+  cat.update(
+    {
+      sleepy: req.body.sleepy
+    },
+    condition,
+    result => {
+      if (result.changedRows == 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+      } 
       res.status(200).end();
     }
-  });
+    });
 });
 
-router.delete("/api/cats/:id", function(req, res) {
+router.delete("/api/cats/:id", (req, res) => {
   var condition = "id = " + req.params.id;
 
-  cat.delete(condition, function(result) {
+  cat.delete(condition, result => {
     if (result.affectedRows == 0) {
       // If no rows were changed, then the ID must not exist, so 404
       return res.status(404).end();
-    } else {
-      res.status(200).end();
     }
+    res.status(200).end();
   });
 });
 
--- public/js/API.js
+++ public/js/API.js
@@ -1,149 +1,114 @@
-const question = document.getElementById('question');
-const choices = Array.from(document.getElementsByClassName('choice-text'));
-const progressText = document.getElementById('progressText');
-const scoreText = document.getElementById('score');
-const progressBarFull = document.getElementById('progressBarFull');
+const question = document.getElementById("question");
+const choices = Array.from(document.getElementsByClassName("choice-text"));
+const progressText = document.getElementById("progressText");
+const scoreText = document.getElementById("score");
+const progressBarFull = document.getElementById("progressBarFull");
 const scoreDiv = document.getElementById("hud-item");
-let currentQuestion = {};
-let acceptingAnswers = false;
-let score = 0;
-let questionCounter = 0;
-let availableQuesions = [];
+const currentQuestion = {};
+const acceptingAnswers = false;
+const score = 0;
+const questionCounter = 0;
+const availableQuesions = [];
 
 let questions = [];
 
-fetch(
-    'https://opentdb.com/api.php?amount=5&category=21&type=multiple'
-)
-    .then((res) => {
-        return res.json();
-    })
-    .then((loadedQuestions) => {
-        questions = loadedQuestions.results.map((loadedQuestion) => {
-            const formattedQuestion = {
-                question: loadedQuestion.question,
-            };
+fetch("https://opentdb.com/api.php?amount=5&category=23&type=multiple")
+  .then(res => {
+    return res.json();
+  })
+  .then(loadedQuestions => {
+    questions = loadedQuestions.results.map(loadedQuestion => {
+      const formattedQuestion = {
+        question: loadedQuestion.question
+      };
 
-            const answerChoices = [...loadedQuestion.incorrect_answers];
-            formattedQuestion.answer = Math.floor(Math.random() * 4) + 1;
-            answerChoices.splice(
-                formattedQuestion.answer - 1,
-                0,
-                loadedQuestion.correct_answer
-            );
+      const answerChoices = [...loadedQuestion.incorrect_answers];
+      formattedQuestion.answer = Math.floor(Math.random() * 4) + 1;
+      answerChoices.splice(
+        formattedQuestion.answer - 1,
+        0,
+        loadedQuestion.correct_answer
+      );
 
-            answerChoices.forEach((choice, index) => {
-                formattedQuestion['choice' + (index + 1)] = choice;
-            });
+      answerChoices.forEach((choice, index) => {
+        formattedQuestion["choice" + (index + 1)] = choice;
+      });
 
-            return formattedQuestion;
-        });
+      return formattedQuestion;
     });
-        fetch(
-            'https://opentdb.com/api.php?amount=5&category=23&type=multiple'
-        )
-            .then((res) => {
-                return res.json();
-            })
-            .then((loadedQuestions) => {
-                questions = loadedQuestions.results.map((loadedQuestion) => {
-                    const formattedQuestion = {
-                        question: loadedQuestion.question,
-                    };
-        
-                    const answerChoices = [...loadedQuestion.incorrect_answers];
-                    formattedQuestion.answer = Math.floor(Math.random() * 4) + 1;
-                    answerChoices.splice(
-                        formattedQuestion.answer - 1,
-                        0,
-                        loadedQuestion.correct_answer
-                    );
-        
-                    answerChoices.forEach((choice, index) => {
-                        formattedQuestion['choice' + (index + 1)] = choice;
-                    });
-        
-                    return formattedQuestion;
-                });
-            });
-                fetch(
-                    'https://opentdb.com/api.php?amount=5&category=28&type=multiple'
-                )
-                    .then((res) => {
-                        return res.json();
-                    })
-                    .then((loadedQuestions) => {
-                        questions = loadedQuestions.results.map((loadedQuestion) => {
-                            const formattedQuestion = {
-                                question: loadedQuestion.question,
-                            };
-                
-                            const answerChoices = [...loadedQuestion.incorrect_answers];
-                            formattedQuestion.answer = Math.floor(Math.random() * 4) + 1;
-                            answerChoices.splice(
-                                formattedQuestion.answer - 1,
-                                0,
-                                loadedQuestion.correct_answer
-                            );
-                
-                            answerChoices.forEach((choice, index) => {
-                                formattedQuestion['choice' + (index + 1)] = choice;
-                            });
-                
-                            return formattedQuestion;
-                        });
-                    });
-                        fetch(
-                            'https://opentdb.com/api.php?amount=5&category=22&type=multiple'
-                        )
-                            .then((res) => {
-                                return res.json();
-                            })
-                            .then((loadedQuestions) => {
-                                questions = loadedQuestions.results.map((loadedQuestion) => {
-                                    const formattedQuestion = {
-                                        question: loadedQuestion.question,
-                                    };
-                        
-                                    const answerChoices = [...loadedQuestion.incorrect_answers];
-                                    formattedQuestion.answer = Math.floor(Math.random() * 4) + 1;
-                                    answerChoices.splice(
-                                        formattedQuestion.answer - 1,
-                                        0,
-                                        loadedQuestion.correct_answer
-                                    );
-                        
-                                    answerChoices.forEach((choice, index) => {
-                                        formattedQuestion['choice' + (index + 1)] = choice;
-                                    });
-                        
-                                    return formattedQuestion;
-                                });
-                            });
-                                fetch(
-                                    'https://opentdb.com/api.php?amount=5&type=multiple'
-                                )
-                                    .then((res) => {
-                                        return res.json();
-                                    })
-                                    .then((loadedQuestions) => {
-                                        questions = loadedQuestions.results.map((loadedQuestion) => {
-                                            const formattedQuestion = {
-                                                question: loadedQuestion.question,
-                                            };
-                                
-                                            const answerChoices = [...loadedQuestion.incorrect_answers];
-                                            formattedQuestion.answer = Math.floor(Math.random() * 4) + 1;
-                                            answerChoices.splice(
-                                                formattedQuestion.answer - 1,
-                                                0,
-                                                loadedQuestion.correct_answer
-                                            );
-                                
-                                            answerChoices.forEach((choice, index) => {
-                                                formattedQuestion['choice' + (index + 1)] = choice;
-                                            });
-                                
-                                            return formattedQuestion;
-                                        });
-                                    });
+  });
+fetch("https://opentdb.com/api.php?amount=5&category=28&type=multiple")
+  .then(res => {
+    return res.json();
+  })
+  .then(loadedQuestions => {
+    questions = loadedQuestions.results.map(loadedQuestion => {
+      const formattedQuestion = {
+        question: loadedQuestion.question
+      };
+
+      const answerChoices = [...loadedQuestion.incorrect_answers];
+      formattedQuestion.answer = Math.floor(Math.random() * 4) + 1;
+      answerChoices.splice(
+        formattedQuestion.answer - 1,
+        0,
+        loadedQuestion.correct_answer
+      );
+
+      answerChoices.forEach((choice, index) => {
+        formattedQuestion["choice" + (index + 1)] = choice;
+      });
+
+      return formattedQuestion;
+    });
+  });
+fetch("https://opentdb.com/api.php?amount=5&category=22&type=multiple")
+  .then(res => {
+    return res.json();
+  })
+  .then(loadedQuestions => {
+    questions = loadedQuestions.results.map(loadedQuestion => {
+      const formattedQuestion = {
+        question: loadedQuestion.question
+      };
+
+      const answerChoices = [...loadedQuestion.incorrect_answers];
+      formattedQuestion.answer = Math.floor(Math.random() * 4) + 1;
+      answerChoices.splice(
+        formattedQuestion.answer - 1,
+        0,
+        loadedQuestion.correct_answer
+      );
+
+      answerChoices.forEach((choice, index) => {
+        formattedQuestion["choice" + (index + 1)] = choice;
+      });
+
+      return formattedQuestion;
+    });
+  });
+fetch("https://opentdb.com/api.php?amount=5&type=multiple")
+  .then(res => {
+    return res.json();
+  })
+  .then(loadedQuestions => {
+    questions = loadedQuestions.results.map(loadedQuestion => {
+      const formattedQuestion = {
+        question: loadedQuestion.question
+      };
+
+      const answerChoices = [...loadedQuestion.incorrect_answers];
+      formattedQuestion.answer = Math.floor(Math.random() * 4) + 1;
+      answerChoices.splice(
+        formattedQuestion.answer - 1,
+        0,
+        loadedQuestion.correct_answer
+      );
+
+      answerChoices.forEach((choice, index) => {
+        formattedQuestion["choice" + (index + 1)] = choice;
+      });
+
+      return formattedQuestion;
+    });
+  });
